9a070d93f997094c700fe3117f22d5b991bc0ea0
Sachin <sachinkshuklaoo7@gmail.com>
2023-04-10 09:08:08 +0530

    Added Events

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -4,4 +4,34 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Donation {
   constructor() public {
   }
+
+  // Keep track of total no. of images
+  uint256 public imageCount = 0;
+
+  // Image data to store
+  struct Image {
+    uint256 id;
+    string hash;
+    string description;
+    uint256 donationAmount;
+    address payable author;
+  }
+
+  mapping(uint256 => Image) public images;
+
+  event ImageCreated(
+    uint256 id,
+    string hash,
+    string description,
+    uint256 donationAmount,
+    address payable author
+  );
+
+  event DonateImage (
+    uint256 id,
+    string hash,
+    string description,
+    uint256 donationAmount,
+    address payable author 
+  );
 }
